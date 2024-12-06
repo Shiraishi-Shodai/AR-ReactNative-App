c313c862b7b44eb1f5c0285d617d41b296a37946
Shiraishi-Shodai <siranosuke1227@gmail.com>
2024-12-06 21:48:32 +0900

    ENHANCEMENT: 円の中心の上半分か下半分で指を離したか判定できるようになった

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from "react";
 import { ViroARSceneNavigator } from "@reactvision/react-viro";
-
 import {
   StyleSheet,
   Text,
@@ -18,24 +17,61 @@ import ControllerModal from "@/components/ControllerModal";
 
 export default () => {
   const { user } = useContext(AuthContext);
-
   // モーダルの表示非表示をコントロールするステート
   const [modlaVisible, setModalVisible] = useState<boolean>(false);
   // 押した場所を保持
   const [position, setPosition] = useState({ x: 0, y: 0 }); // 座標を管理
-  // スクリーンのサイズを取得
+  // スクリーンのサイズを取得(ピクセル単位)
   const { height, width } = useWindowDimensions();
+  // コントロールを表示するエリア
+  const controllArea = {
+    width: 200,
+    height: 200,
+    radius: 100,
+  };
 
   // 長押しの状態が変化したときの処理
   const handleStateChange = (event: any) => {
-    if (event.nativeEvent.state === State.ACTIVE) {
-      const { absoluteX, absoluteY } = event.nativeEvent;
-      setPosition({ x: absoluteX, y: absoluteY }); // 座標を更新
-      setModalVisible(true);
-    } else if (event.nativeEvent.state === State.END) {
-      setModalVisible(false);
-    } else if (event.nativeEvent.state === State.CANCELLED) {
-      setModalVisible(false);
+    // 指の状態、指の位置のX座標、指の位置のY座標
+    const { state, absoluteX, absoluteY } = event.nativeEvent;
+
+    switch (state) {
+      case State.ACTIVE: // 長押しをしたとき
+        // タップをした位置にビューの中心がくるような位置を求めてセットする
+        setPosition({
+          x: absoluteX - controllArea.width / 2,
+          y: absoluteY - controllArea.height / 2,
+        });
+        // モーダルを表示する
+        setModalVisible(true);
+        break;
+      case State.END: // 長押しをして指を離したとき
+        // ビューの中心の座標 = タップをした位置 を求める
+        const controllAreaCenter = {
+          x: position.x + controllArea.width / 2,
+          y: position.y + controllArea.height / 2,
+        };
+        // 円の中心から指を離した位置までのユーグリッド距離を求める
+        const distance = Math.sqrt(
+          Math.pow(absoluteX - controllAreaCenter.x, 2) +
+            Math.pow(absoluteY - controllAreaCenter.y, 2)
+        );
+        // 円の内側で指を離したか？
+        if (distance <= controllArea.radius) {
+          console.log("エリア内で指が離されました");
+          // 円の上半分で指を離したか？
+          if (absoluteY <= controllAreaCenter.y) {
+            console.log("上半分で指を離しました");
+          } else {
+            console.log("下半分で指を離しました");
+          }
+        }
+        // モーダルを閉じる
+        setModalVisible(false);
+        break;
+      case State.CANCELLED: // 長押し中に移動可能な範囲を超えた時
+        // モーダルを閉じる
+        setModalVisible(false);
     }
   };
 
@@ -59,6 +95,9 @@ export default () => {
           setModalVisible={setModalVisible}
           x={position.x}
           y={position.y}
+          width={controllArea.width}
+          height={controllArea.height}
+          radius={controllArea.radius}
         />
       </View>
     </LongPressGestureHandler>
--- components/ControllerModal.tsx
+++ components/ControllerModal.tsx
@@ -1,17 +1,24 @@
 import React, { SetStateAction } from "react";
 import { Modal, StyleSheet, Text, View } from "react-native";
+import { TapGestureHandler } from "react-native-gesture-handler";
 
 interface ControllerModalProps {
   modlaVisible: boolean;
   setModalVisible: React.Dispatch<SetStateAction<boolean>>;
   x: number;
   y: number;
+  width: number;
+  height: number;
+  radius: number;
 }
 const ControllerModal = ({
   modlaVisible,
   setModalVisible,
   x,
   y,
+  width,
+  height,
+  radius,
 }: ControllerModalProps) => {
   return (
     <Modal transparent={true} visible={modlaVisible}>
@@ -21,14 +28,12 @@ const ControllerModal = ({
           {
             top: y, // 長押しされたY座標
             left: x, // 長押しされたX座標
+            width: width, // ビューの幅
+            height: height, // ビューの高さ
+            borderRadius: radius, // 四つ角の丸み
           },
         ]}
-        onStartShouldSetResponder={() => true}
-        onResponderRelease={() => console.log("ここで離されました")}
-        onResponderTerminate={() => console.log("onResponderTerminate")}
-      >
-        <Text style={styles.popupText}>ここを長押ししました！</Text>
-      </View>
+      ></View>
     </Modal>
   );
 };
@@ -40,9 +45,6 @@ const styles = StyleSheet.create({
     position: "absolute",
     backgroundColor: "white",
     padding: 10,
-    borderRadius: 100,
-    width: 200,
-    height: 200,
   },
   popupText: {
     color: "white",
